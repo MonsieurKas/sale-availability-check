91c655c91d9c21f478579ecaee0951382228e2dc
MonsieurKas <kinglargo@gmail.com>
2021-01-23 11:29:59 +0100

    adding mail notification

--- src/search.js
+++ src/search.js
@@ -1,18 +1,28 @@
 // Import website you want to use
+const { RueDuCommerce } = require('./websites/rue-du-commerce');
 
 // TO IGNORE
-const defaultRetryTimer = 90e3; // = 90 seconds
+const defaultRetryTimer = 300e3; // = 90 seconds
 
 // STUFF YOU CAN CUSTOMIZE
 const customRetryTimer = null; // Set the value you want
 
 const searchArray = [
-  // Put here whatever's suggested in the website you want to check's README file
+  { articleName: '3080 RTX', search: '3080-rtx-10go' },
+  { articleName: '3070 RTX', search: '3070-rtx-8go' },
+  { articleName: '3080 RTX', search: '3080-rtx' },
+  { articleName: '3070 RTX', search: '3070-rtx' },
+  { articleName: '3080 RTX', search: '3080' },
+  { articleName: '3070 RTX', search: '3070' },
+  // { articleName: 'GT 710', search: 'gt-710' }
 ];
+const rueDuCommerce = new RueDuCommerce(searchArray);
 
 const searchFn = () => {
   const searches = [
     // Put here whatever's suggested in the website you want to check's README file
+    rueDuCommerce.runChecks(),
+    // rueDuCommerce.sendMailTo()
   ];
 
   if (!searchArray.length) return console.log(`You forgot to setup in the searchArray. Check the README files for more info.`);
@@ -23,6 +33,7 @@ const searchFn = () => {
     .then((_) => {
       console.log();
       setTimeout(searchFn, customRetryTimer || defaultRetryTimer);
+      rueDuCommerce.sendMailTo()
     });
 };
 
--- src/websites/rue-du-commerce/index.js
+++ src/websites/rue-du-commerce/index.js
@@ -1,25 +1,54 @@
 const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 const colors = require('colors');
+const nodemailer = require('nodemailer');
 
 const { urlBuilder } = require('../../utils');
 
 const HANDLE = 'RDV';
 const URL = 'https://www.rueducommerce.fr';
 
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'autobot.ramani@gmail.com',
+    pass: "Maison4802+"
+  }
+});
+
 // Read the README file for info
 class RueDuCommerce {
-  constructor (searchArray, options) {
-    this.searches = paramsBuilder(searchArray);
+  constructor(searchArray, options) {
+    this.searches = paramsBuilder(searchArray)
+    this.i = 0
+    this.cardArray = ""
   }
 
-  runChecks () {
+  runChecks() {
     return this.searches.map(({ url, path, articleName }) => fetch(urlBuilder(url, path))
       .then(this.getLinks)
       .then((links) => this.getAvailability(links, url, articleName)))
   }
 
-  getLinks (response) {
+  sendMailTo(){
+    if (this.i > 0) {
+      var mailOptions = {
+        from: 'autobot.ramani@gmail.com',
+        to: 'kinglargo@gmail.com',
+        subject: `'Found ${this.i} Card(s)!`,
+        html: this.cardArray
+      };
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      })
+    }
+  }
+
+  getLinks(response) {
     return response.text()
       .then((html) => {
         const $ = cheerio.load(html);
@@ -27,21 +56,25 @@ class RueDuCommerce {
       })
   }
 
-  getAvailability (links, url, articleName) {
+  getAvailability(links, url, articleName) {
     return Promise.all(links.map((link) => {
       return fetch(urlBuilder(url, link))
         .then(getHtml)
         .then((html) => !cheerio.load(html)('#product_action a').first().toString().includes('display:none'))
         .then((isAvailable) => {
+
           if (isAvailable) {
             console.log(`${articleName}:`, `${urlBuilder(url, link)}`.white.bgGreen);
+            this.i++
+            this.cardArray += `${articleName}: ${urlBuilder(url, link)}<br>`
           } else {
-            console.log(`${articleName}:`, `${urlBuilder(url, link)}`.white.bgRed);
+            console.log(`${articleName}:`, `${urlBuilder(url, link)}`.white);
           }
-          return isAvailable;
-        });
-      }));
-    }
+          return isAvailable
+        })
+    }))
+
+  }
 }
 
 const getHtml = (res) => res.text();
